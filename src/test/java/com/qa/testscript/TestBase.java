8101f1bccf872a8a5ca7da677e94845ea5760851
Meghanavarma10 <89564162+Meghanavarma10@users.noreply.github.com>
2021-09-20 18:40:08 +0530

    Create TestBase.java

--- src/test/java/com/qa/testscript/TestBase.java
+++ src/test/java/com/qa/testscript/TestBase.java
@@ -1,60 +1,34 @@
 package com.qa.testscript;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-<<<<<<< HEAD
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.ITestContext;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
-import com.qa.pages.AddToCart_Pages;
-import com.qa.pages.Address_Pages;
-import com.qa.pages.Cart_ByProducts__Pages;
-import com.qa.pages.Update_Password_Pages;
+import com.qa.pages.support_pages;
 
-public class TestBase {
-	public WebDriver driver;
-	AddToCart_Pages addToCart_Pages;
-    Cart_ByProducts__Pages cart_ByProducts__Pages;
-    Address_Pages address_Pages;
-    Update_Password_Pages update_Password_Pages;
-    @BeforeClass
-	public void setup(ITestContext context)
-	{
-    	System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\HP\\eclipse-workspace-latest\\Application_Mystore_Automation\\chromedriver\\chromedriver.exe");
-		driver=new ChromeDriver();
-		driver.get("http://automationpractice.com/index.php");
-		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-		driver.manage().window().maximize();
-		addToCart_Pages=new AddToCart_Pages(driver);
-		cart_ByProducts__Pages=new Cart_ByProducts__Pages(driver);
-		address_Pages=new Address_Pages(driver);
-		update_Password_Pages=new Update_Password_Pages(driver);
-		context.setAttribute("WebDriver", driver);
-		addToCart_Pages.getSignin().click();
-		addToCart_Pages.getEmail().sendKeys("sai10091997@gmail.com");
-		addToCart_Pages.getPassword().sendKeys("596844");
-		addToCart_Pages.getSubmit_btn().click();
-		
+public class TestBase<signin_pages> {
 
-		
-	}
-    
-   
-    @AfterClass
-	public void teardown() throws InterruptedException
-	{
-    	Thread.sleep(3000);
-		driver.close();
-	}
-=======
-import org.testng.annotations.AfterClass;
+	static WebDriver driver;
+	
+	support_pages support_pages ;
 
-import org.testng.annotations.BeforeClass;
->>>>>>> 62e6ccc8986a05eb54d56110bd514a40faf3a7a2
+@BeforeClass
+public WebDriver setup() {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Meghana\\Desktop\\selenium software\\chromedriver.exe");
+	driver=new ChromeDriver();
+	driver.get("http://automationpractice.com/index.php");
+	return driver;
+	
+}
+	
+	   @AfterClass
+	    public void tearDown() {
+	    	//close browser
+	    	driver.close();
+	    }
 
+	  
+}
